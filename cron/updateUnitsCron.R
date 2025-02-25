f9a481682c30bbb2c3dbfc2113355274a5a1233d
Rosa Maggi <rosa.maggi@epfl.ch>
2025-02-25 14:03:13 +0100

    [feature] activate  environment variables for container

--- cron/updateUnitsCron.R
+++ cron/updateUnitsCron.R
@@ -7,15 +7,15 @@ library(jsonlite)
 library(stringr)
 
 # Connect to my-db as defined in /etc/mysql/my.cnf
-con <- dbConnect(RMariaDB::MariaDB(), default.file = '/etc/mysql/my.cnf', group = "lhd")
+# con <- dbConnect(RMariaDB::MariaDB(), default.file = '/etc/mysql/my.cnf', group = "lhd")
 
-# dbNameVar <- Sys.getenv("MYSQL_LHD_V2_DBNAME")
-# dbHostVar <- Sys.getenv("MYSQL_LHD_V2_HOST")
-# dbPasswordVar <- Sys.getenv("MYSQL_LHD_V2_PASSWORD")
-# dbPortVar <- Sys.getenv("MYSQL_LHD_V2_PORT")
-# dbUserVar <- Sys.getenv("MYSQL_LHD_V2_USER")
-# lhdApiPassword <- Sys.getenv("LHD_API_PASSWORD")
-# con <- dbConnect(RMariaDB::MariaDB(), username = dbUserVar, password = dbPasswordVar, host = dbHostVar, port = dbPortVar, dbname = dbNameVar)
+dbNameVar <- Sys.getenv("MYSQL_LHD_V2_DBNAME")
+dbHostVar <- Sys.getenv("MYSQL_LHD_V2_HOST")
+dbPasswordVar <- Sys.getenv("MYSQL_LHD_V2_PASSWORD")
+dbPortVar <- Sys.getenv("MYSQL_LHD_V2_PORT")
+dbUserVar <- Sys.getenv("MYSQL_LHD_V2_USER")
+lhdApiPassword <- Sys.getenv("LHD_API_PASSWORD")
+con <- dbConnect(RMariaDB::MariaDB(), username = dbUserVar, password = dbPasswordVar, host = dbHostVar, port = dbPortVar, dbname = dbNameVar)
 
 getScipersFromLhd <- function(con) {
   scipersId <- tbl(con, "unit") %>% 
