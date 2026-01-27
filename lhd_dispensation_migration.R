e515ef4f0f660a9059ad27f41d47bd49066accfe
Rosa Maggi <rosa.maggi@epfl.ch>
2026-01-27 11:57:24 +0100

    [feature] Add dispensation migration

---
+++ lhd_dispensation_migration.R
@@ -0,0 +1,70 @@
+library(RMariaDB)
+library(DBI)
+library(dplyr)
+library(dbplyr)
+library(stringr)
+library(httr)
+library(jsonlite)
+
+# Connect to my-db as defined in /etc/mysql/my.cnf
+# con <- dbConnect(RMariaDB::MariaDB(), default.file = '/etc/mysql/my.cnf', group = "lhd")
+
+dbNameVar <- Sys.getenv("MYSQL_LHD_V2_DBNAME")
+dbHostVar <- Sys.getenv("MYSQL_LHD_V2_HOST")
+dbPasswordVar <- Sys.getenv("MYSQL_LHD_V2_PASSWORD")
+dbPortVar <- Sys.getenv("MYSQL_LHD_V2_PORT")
+dbUserVar <- Sys.getenv("MYSQL_LHD_V2_USER")
+lhdApiPassword <- Sys.getenv("LHD_API_PASSWORD")
+con <- dbConnect(RMariaDB::MariaDB(), username = dbUserVar, password = dbPasswordVar, host = dbHostVar, port = dbPortVar, dbname = dbNameVar)
+
+print(con)
+
+statusToImport <- c('Expired','Active')
+now <- Sys.time()
+
+latest_versions <- tbl(con, "auth_dsps_version") %>%
+  group_by(id_auth_dsps) %>%
+  summarise(max_version_id = max(id_auth_dsps_version, na.rm = TRUE)) %>%
+  ungroup()
+
+dispensations <- tbl(con, "auth_dsps") %>%
+  inner_join(tbl(con, "auth_dsps_version"), by = 'id_auth_dsps') %>%
+  inner_join(latest_versions, by = 'id_auth_dsps') %>%
+  inner_join(tbl(con, "dispensation_subject"), by = 'subject') %>%
+  filter(id_auth_dsps_version == max_version_id) %>%
+  filter(status %in% statusToImport) %>%
+  mutate(
+    renewals = 0,
+    modified_by = 'Import',
+    modified_on = now,
+    other_subject = ''
+    ) %>%
+  select('id_auth_dsps' , 'auth_dsps', 'author' , 'id_dispensation_subject' ,'requires' , 'comment', 'status', 'date_start', 'date_end', 'date', 'renewals', 'modified_by', 'modified_on', 'other_subject') %>%
+  rename(id_dispensation = id_auth_dsps ) %>%
+  rename(dispensation = auth_dsps ) %>%
+  rename(created_by = author ) %>%
+  rename(created_on = date ) %>%
+  collect()
+
+dbAppendTable(con, 'dispensation', dispensations)
+
+rooms <- tbl(con, "auth_dsps_lab") %>%
+  inner_join(latest_versions, by = c("id_auth_dsps_version" = "max_version_id")) %>%
+  inner_join(tbl(con, "auth_dsps_version"), by = 'id_auth_dsps_version') %>%
+  filter(status %in% statusToImport) %>%
+  select(id_lab, id_dispensation = id_auth_dsps.x) %>%
+  collect()
+
+dbAppendTable(con, 'dispensation_has_room', rooms)
+
+holders <- tbl(con, "auth_dsps_holder") %>%
+  inner_join(latest_versions, by = c("id_auth_dsps_version" = "max_version_id")) %>%
+  inner_join(tbl(con, "auth_dsps_version"), by = 'id_auth_dsps_version') %>%
+  filter(status %in% statusToImport) %>%
+  select(id_person, id_dispensation = id_auth_dsps.x) %>%
+  collect()
+
+dbAppendTable(con, 'dispensation_has_holder', holders)
+
+# Disconnect from the database
+dbDisconnect(con)
